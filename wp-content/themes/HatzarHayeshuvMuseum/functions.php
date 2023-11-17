bae041e4d9d0411db3cacdf4c38466c1d4ed5895
admin <ilay.skutelsky@gmail.com>
2023-11-17 15:10:16 +0200

    modified theme ``

--- wp-content/themes/HatzarHayeshuvMuseum/functions.php
+++ wp-content/themes/HatzarHayeshuvMuseum/functions.php
@@ -111,3 +111,105 @@ add_action( 'rest_api_init', 'register_rest_images' );
 wp_using_ext_object_cache(false);
 wp_cache_flush();
 wp_cache_init();
+
+
+
+add_action('acf/save_post', 'add_other_selected_options_to_fields');
+function add_other_selected_options_to_fields( $post_id ) {
+
+    // Get newly saved values.
+    $values = get_fields( $post_id );
+	consolelog($values);
+	
+	// https://support.advancedcustomfields.com/forums/topic/updating-field-settings-in-php/
+
+	$new_color = in_array("אחר", $values['characteristics']['colors'])? $values['characteristics']['צבע_אחר'] : false;
+	if ($new_color) add_new_option_to_field_choices('colors', $new_color);
+
+	$new_field = $values['characteristics']['field'] == "אחר"? $values['characteristics']['תחום_אחר'] : false;
+	if ($new_field) add_new_option_to_field_choices('field', $new_field);
+
+	$new_ethnicity = $values['characteristics']['ethnicity'] == "אחר"? $values['characteristics']['עדה_אחר'] : false;
+	if ($new_ethnicity) add_new_option_to_field_choices('ethnicity', $new_ethnicity);
+
+	$new_material = in_array("אחר", $values['characteristics']['materials'])? $values['characteristics']['חומרים_אחר'] : false;
+	if ($new_material) add_new_option_to_field_choices('materials', $new_material);
+
+	$new_technique = in_array("אחר", $values['characteristics']['techniques'])? $values['characteristics']['טכניקות_אחר'] : false;
+	if ($new_technique) add_new_option_to_field_choices('techniques', $new_technique);
+
+	$new_status = $values['status'] == "אחר"? $values['סטטוס_אחר'] : false;
+	if ($new_status) add_new_option_to_field_choices('status', $new_status);
+
+	$new_period = $values['תקופה'] == "אחר"? $values['תקופה_אחר'] : false;
+	if ($new_period) add_new_option_to_field_choices('תקופה', $new_period);
+
+	if ($new_color || $new_field || $new_ethnicity || $new_metrial || $new_technique) {
+
+		if ($new_color) {
+			$new_color_array = $values['characteristics']['colors'];
+			array_push($new_color_array, $new_color);
+			// $new_color = [...$values['characteristics']['colors'], $new_color];
+			$new_color_array = array_filter($new_color_array, fn($n) => $n !== "אחר");
+			$new_color = $new_color_array;
+		}
+		if ($new_material) {
+			$new_material_array = $values['characteristics']['materials'];
+			array_push($new_material_array, $new_material);
+			// $new_material = [...$values['characteristics']['materials'], $new_materials];
+			$new_material_array = array_filter($new_material_array, fn($n) => $n !== "אחר");
+			$new_material = $new_material_array;
+		}
+		if ($new_technique) {
+			$new_technique_array = $values['characteristics']['techniques'];
+			array_push($new_technique_array, $new_technique);
+			// $new_ethnicity = [...$values['characteristics']['ethnicity'], $new_ethnicity];
+			$new_technique_array = array_filter($new_technique_array, fn($n) => $n !== "אחר");
+			$new_technique = $new_technique_array;
+		}
+
+		$charects = Array(
+	       'field_6533cbd4c6d1c' => $new_color? $new_color : $values['characteristics']['colors'],
+	       'field_6533cc47c6d1d' => $new_field? $new_field : $values['characteristics']['field'],
+	       'field_6533cc77c6d1e' => $new_ethnicity? $new_ethnicity : $values['characteristics']['ethnicity'],
+	       'field_6533ccadc6d20' => $new_material? $new_material : $values['characteristics']['materials'],
+	       'field_6533ccccc6d21' => $new_technique? $new_technique : $values['characteristics']['techniques'],
+
+	       'field_6533cd22c6d22' => $values['characteristics']['size'],
+
+		   'field_6554ea526be07' => '',
+		   'field_65524e61c9fa8' => '',
+		   'field_65524e85c9fa9' => '',
+		   'field_6554e9efd57d9' => '',
+		   'field_65524e99c9faa' => ''
+		);
+	    update_field('field_6533cb66c6d18', $charects, $post_id);
+	}
+
+	if ($new_status) {
+	    update_field('field_653406eef9701', $new_status, $post_id);
+	    update_field('field_65524e25c9fa7', '', $post_id);
+	}
+	
+	if ($new_period) {
+	    update_field('field_6533ac6e66ff3', $new_period, $post_id);
+	    update_field('field_65524df8c9fa6', '', $post_id);
+	}
+	
+ 	echo "<script>history.go(-2);</script>";
+}
+
+function add_new_option_to_field_choices($field_name, $new_value) {
+	$field = acf_get_field($field_name, true);
+	if( !isset($field['choices'][$new_value]) ) {
+		$field['choices'][$new_value] = $new_value;
+		acf_update_field($field);
+	}
+
+}
+
+function consolelog($data) {
+    $output = json_encode($data);
+
+    echo "<script>console.log('Debug Objects:');console.log(JSON.parse(`" . $output . "`.replace(/[\u0000-\u001F\u007F-\u009F]/g, '')));</script>";
+}
