83f7b6adb307addcaf10026bda616ad23db8bad7
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2024-01-23 13:02:38 +0200

    Bugfix recursion limit and exhib inclusion

--- wp-content/themes/HatzarHayeshuvMuseum/assets/js/admin.js
+++ wp-content/themes/HatzarHayeshuvMuseum/assets/js/admin.js
@@ -2,18 +2,18 @@ recursiveChangeID(0)
 
 function recursiveChangeID(count) {
     if (count >= 100) return
-    const itemsRelationshipFieldElm = document.querySelector('.acf-field.acf-field-relationship[data-type="relationship"][data-name="items"]')
+    const itemsRelationshipFieldElm = document.querySelector('.acf-field.acf-field-relationship[data-type="relationship"][data-name$="items"]')
     if (itemsRelationshipFieldElm) {
         const itemAddRowElms = itemsRelationshipFieldElm.querySelectorAll('.acf-input .selection ul li span.acf-rel-item.acf-rel-item-add')
         const itemRemoveRowElms = itemsRelationshipFieldElm.querySelectorAll('.acf-input .selection ul li span.acf-rel-item.acf-rel-item-remove')
-        if (!itemAddRowElms.length || !itemRemoveRowElms.length) return setTimeout(recursiveChangeID, 200, count++)
+        if (!itemAddRowElms.length || !itemRemoveRowElms.length) return setTimeout(recursiveChangeID, 200, ++count)
         let itemRowElms = Array.from(itemAddRowElms).concat(Array.from(itemRemoveRowElms))
         itemRowElms.forEach(itemRowElm => {
             const id = itemRowElm.dataset.id
             itemRowElm.dataset.id = getCataolgNumberFromID(id)
         })
     } else {
-        return setTimeout(recursiveChangeID, 500, count++)
+        return setTimeout(recursiveChangeID, 500, ++count)
     }
 }
 
